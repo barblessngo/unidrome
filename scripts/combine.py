16c8c930ee41fda81bd0fd3797906d8f7d1e880a
Alex Polvi <alex@polvi.net>
2024-06-03 08:11:46 -0700

    new class model

--- scripts/combine.py
+++ scripts/combine.py
@@ -2,10 +2,21 @@ from lon_lat_lookup_gen import lon_lat_lookup, get_all_gdfs
 import geopandas as gpd
 import pandas as pd
 import h3pandas
+from sklearn.cluster import DBSCAN
+import numpy as np
 
 gdfs = get_all_gdfs()
+for file in gdfs:
+    # filter out closed airports and heliports
+    g = gdfs[file]
+    airports = g[g['_is_airport'] == True]
+    active = airports[airports['_is_active'] == True]
+    gdfs[file] = active
+
+
 prefixes = [ f"{k.replace('data/', '').replace('.csv', '').replace('/', '_').replace('.', '_')}_" for k in lon_lat_lookup ]
 gdfs = [gdfs[file] for file in gdfs]
+all_gdf_cnt = len(gdfs)
 prefixed_gdfs = []
 for gdf, prefix in zip(gdfs, prefixes):
     prefixed_gdf = gdf.rename(columns=lambda x: f"{prefix}{x}" if x != 'geometry' else x)
@@ -15,10 +26,6 @@ gdfs = prefixed_gdfs
 for gdf in gdfs:
     gdf.set_crs(epsg=4326, inplace=True)
 
-for i, gdf in enumerate(gdfs):
-    gdfs[i] = gdf.rename(columns={'geometry': f'geometry_temp_{i}'})
-
-# Rename the geometry column in each GeoDataFrame to avoid conflicts
 for i, gdf in enumerate(gdfs):
     gdfs[i] = gdf.rename(columns={'geometry': f'geometry_temp_{i}'})
 
@@ -36,11 +43,37 @@ combined_gdf = combined_gdf.drop(columns=geometry_cols)
 
 # Convert the combined DataFrame back to a GeoDataFrame
 combined_gdf = gpd.GeoDataFrame(combined_gdf, geometry='geometry')
+#combined_gdf = combined_gdf[combined_gdf['us_faa_nasr_APT_BASE_SITE_TYPE_CODE'] != 'H']
+#combined_gdf = combined_gdf[combined_gdf['world_ourairports_airports_type'] != 'heliport']
+#combined_gdf = combined_gdf[combined_gdf['world_ourairports_airports_type'] != 'closed']
+#combined_gdf = combined_gdf[~combined_gdf['world_osm_daylight_aerodrome_name'].str.contains('heliport', case=False, na=False)]
 
-#df = combined_gdf.h3.geo_to_h3_aggregate(7)
-df = combined_gdf.h3.geo_to_h3(7)
-h3_combined = gpd.GeoDataFrame(df, geometry='geometry')
+#combined_gdf = combined_gdf.h3.geo_to_h3(3)
+gdf = combined_gdf
+coords = np.array(list(zip(gdf.geometry.x, gdf.geometry.y)))
+    
+# Perform DBSCAN clustering
+db = DBSCAN(eps=.005, min_samples=2).fit(coords)
+labels = db.labels_
+gdf['cluster'] = db.labels_
 
-# Save the combined GeoDataFrame to a file
-#h3_combined.to_file("package.gpkg", layer='unidrome', driver="GPKG")
-h3_combined.to_csv("data/combined.csv")
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print("Estimated number of clusters: %d" % n_clusters_)
+print("Estimated number of noise points: %d" % n_noise_)
+
+# Aggregate based on cluster
+singles = gdf[gdf["cluster"] == -1]
+clusters = gdf[gdf["cluster"] != -1].dissolve(by='cluster')
+
+# Convert singles to GeoDataFrame to concatenate
+#singles = gpd.GeoDataFrame(singles, geometry='geometry')
+
+# Concatenate clusters and singles
+clusters = pd.concat([clusters, singles])
+
+# Save the result to a new layer called "clusters"
+clusters.to_file("package.gpkg", layer='clusters', driver="GPKG")
+
--- scripts/lon_lat_lookup_gen.py
+++ scripts/lon_lat_lookup_gen.py
@@ -2,25 +2,111 @@ from shapely.geometry import Point
 import pandas as pd
 import geopandas as gpd
 
+class AerodromeParser():
+
+    def parse_geom(row):
+        return None
+
+    def is_airport(row):
+        return False
+
+    def is_heliport(row):
+        return False
+
+    def is_active(row):
+        return True
+
+class MxParser(AerodromeParser):
+    def parse_geom(row):
+        try:
+            if pd.isna(row.iloc[11]):
+                return None
+            latitude_deg = float(row.iloc[11])
+            latitude_min = float(row.iloc[12])
+            latitude_sec = float(row.iloc[13])
+
+            longitude_deg = float(row.iloc[14])
+            longitude_min = float(row.iloc[15])
+            longitude_sec = float(row.iloc[16])
+            latitude = latitude_deg + latitude_min/60 + latitude_sec/3600
+            longitude = -1 * (longitude_deg + longitude_min/60 + longitude_sec/3600)
+            return Point(longitude, latitude)
+        # if it cannot parse a float, return none
+        except ValueError:
+            return None
+
+    def is_airport(row):
+        return row['TIPO AERÓDROMO'] == 'AERÓDROMO'
+
+    def is_heliport(row):
+        return row['NO. DE EXPEDIENTE'][:2] == 'HP' or row['TIPO AERÓDROMO'] == 'HELIPUERTO'
+
+class FAAParser(AerodromeParser):
+    def parse_geom(row):
+        return Point(float(row.iloc[24]), float(row.iloc[19])) if row.iloc[19] and row.iloc[24] else None
+
+    def is_airport(row):
+        return row['SITE_TYPE_CODE'] != 'H'
+
+    def is_heliport(row):
+        return row['SITE_TYPE_CODE'] == 'H'
+
+    def is_active(row):
+        return True
+
+class OurAirportsParser(AerodromeParser):
+    def parse_geom(row):
+        return Point(float(row.iloc[5]), float(row.iloc[4]))
+
+    def is_airport(row):
+        return row['type'].find('airport') != -1
+
+    def is_heliport(row):
+        return row['type'] == 'heliport'
+
+    def is_active(row):
+        return row['type'] != 'closed'
+
+class OSMDaylightParser(AerodromeParser):
+    def parse_geom(row):
+        return Point(float(row.iloc[2]), float(row.iloc[1]))
+
+    def is_airport(row):
+        if pd.isna(row['name']):
+            return True
+        return row['name'].find('heliport') == -1
+
+    def is_heliport(row):
+        if pd.isna(row['name']):
+            return True
+        return row['name'].find('heliport') != -1
+
+
 lon_lat_lookup = {
-    'data/us/faa/nasr/APT_BASE.csv': lambda row: Point(float(row.iloc[24]), float(row.iloc[19])) if row.iloc[19] and row.iloc[24] else None,
-    #'data/world/ourairports/runways.csv': lambda row: Point(float(row.iloc[10]), float(row.iloc[9])) if row.iloc[9] and row.iloc[10] else None,
-    'data/world/ourairports/airports.csv': lambda row: Point(float(row.iloc[5]), float(row.iloc[4])),
-    #'data/world/osm/daylight/runway.csv': lambda row: Point(float(row.iloc[2]), float(row.iloc[1])),
-    'data/world/osm/daylight/aerodrome.csv': lambda row: Point(float(row.iloc[2]), float(row.iloc[1])),
-    'data/world/wikidata/airports.csv': lambda row: Point(float(row.iloc[8]), float(row.iloc[9])),
+    'data/mx/afac/aerodromos.csv': MxParser,
+    'data/us/faa/nasr/APT_BASE.csv': FAAParser,
+    ##'data/world/ourairports/runways.csv': lambda row: Point(float(row.iloc[10]), float(row.iloc[9])) if row.iloc[9] and row.iloc[10] else None,
+    'data/world/ourairports/airports.csv': OurAirportsParser,
+    ##'data/world/osm/daylight/runway.csv': lambda row: Point(float(row.iloc[2]), float(row.iloc[1])),
+    'data/world/osm/daylight/aerodrome.csv': OSMDaylightParser,
+    #'data/world/wikidata/airports.csv': lambda row: Point(float(row.iloc[8]), float(row.iloc[9])),
 }
+
 
 def get_gdf(file_path):
     # Read the CSV file into a DataFrame
     df = pd.read_csv(file_path, low_memory=False)
     
+    parser = lon_lat_lookup[file_path]
     # Apply the lambda function to each row
-    df['geometry'] = df.apply(lon_lat_lookup[file_path], axis=1)
+    df['geometry'] = df.apply(parser.parse_geom, axis=1)
     df = df.dropna(subset=['geometry'])
-    gfd = gpd.GeoDataFrame(df)
+    df['_is_airport'] = df.apply(parser.is_airport, axis=1)
+    df['_is_heliport'] = df.apply(parser.is_heliport, axis=1)
+    df['_is_active'] = df.apply(parser.is_active, axis=1)
+    gdf = gpd.GeoDataFrame(df)
     
-    return gfd
+    return gdf
 
 def get_all_gdfs():
     gdfs = {}
